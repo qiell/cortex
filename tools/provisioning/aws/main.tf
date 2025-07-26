7f59c3634dbe78a05743350bf39b8b03f8f52a17
Akash Srivastava <akashsrivastava4927@gmail.com>
2025-07-27 01:55:59 +0530

    Automated remediation of security findings

--- tools/provisioning/aws/main.tf
+++ tools/provisioning/aws/main.tf
@@ -17,9 +17,9 @@ resource "aws_security_group" "allow_ssh" {
   }
 
   egress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -125,6 +125,14 @@ resource "aws_instance" "tf_test_vm" {
 
   # Enable detailed monitoring
   monitoring = true
+
+  # Disable IMDS or require IMDSv2
+  metadata_options {
+    http_tokens = "required"
+  }
+
+  # Specify the non-default VPC
+  vpc_security_group_ids = ["${aws_security_group.example.id}"]
 
   # Wait for machine to be SSH-able:
   provisioner "remote-exec" {
