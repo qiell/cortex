a5839b46d1cda386ca76d73dbc56d66d74e5eb93
Akash Srivastava <akashsrivastava4927@gmail.com>
2025-07-29 23:51:27 +0530

    Automated remediation of security findings

--- tools/provisioning/aws/main.tf
+++ tools/provisioning/aws/main.tf
@@ -17,9 +17,9 @@ resource "aws_security_group" "allow_ssh" {
   }
 
   egress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
